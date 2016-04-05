778130385dcd7e2f8543a4647c0e2c442fa7a7fe
Giacomo Donati <donatigiacomo91@gmail.com>
2016-04-05 10:39:49 +0200

    remove duplicated code

--- sequential.cpp
+++ sequential.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 
 #include "board.h"
+#include "game_logic.h"
 #include "game_conf.h"
 
 /*
@@ -12,34 +13,6 @@
  * */
 
 // #define PRINT
-
-void update(int i, int j, board& in, board& out) {
-
-    // upper and lower row indices
-    auto up_p = (i-1) >= 0 ? (i-1) : (in.m_height-1);
-    auto low_p = (i+1) % in.m_height;
-    // left and right column indices
-    auto left_p = (j-1) >= 0 ? (j-1) : (in.m_width-1);
-    auto right_p = (j+1) % in.m_width;
-
-    auto sum = in[up_p][left_p] + in[up_p][j] + in[up_p][right_p]
-               + in[i][left_p] + in[i][right_p]
-               + in[low_p][left_p] + in[low_p][j] + in[low_p][right_p];
-
-    // empty cell
-    if (in[i][j] == 0) {
-        // with exactly 3 alive neighbours alive otherwise die
-        out[i][j] = (sum == 3) ? 1 : 0;
-        return;
-    }
-    // alive cell
-    if (in[i][j] == 1) {
-        // with less then 2 or more than 3 alive neighbours then die
-        // otherwise keep alive
-        out[i][j] = (sum < 2 || sum > 3) ? 0 : 1;
-        return;
-    }
-}
 
 int main(int argc, char* argv[]) {
 
@@ -87,7 +60,7 @@ int main(int argc, char* argv[]) {
 
         for (auto i = 0; i < rows; ++i) {
             for (auto j = 0; j < cols ; ++j) {
-                update(i,j,*p_in,*p_out);
+                game_logic::update(i,j,*p_in,*p_out);
             }
         }
         #ifdef PRINT
