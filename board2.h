e21fcf4a51a4a5ed93313d03b098bcb280d721de
Giacomo Donati <donatigiacomo91@gmail.com>
2016-04-07 11:54:32 +0200

    sequential cache efficient implementation

---
+++ board2.h
@@ -0,0 +1,88 @@
+//
+// Created by Giacomo Donati on 06/04/16.
+//
+
+#ifndef GOL_BOARD2_H
+#define GOL_BOARD2_H
+
+/* #if defined(__INTEL_COMPILER)
+#include <malloc.h>
+#else
+#include <mm_malloc.h>
+#endif // defined(__GNUC__) */
+
+#include <cstdlib>
+#include <iostream>
+
+// extended board with additional borders
+
+class board2 {
+public:
+    int* matrix;
+    int m_width;
+    int m_height;
+
+    board2(int rows, int columns) {
+        m_width = columns+2;
+        m_height = rows+2;
+        matrix = (int*) malloc(m_width * m_height * sizeof(int));
+        fill_random();
+    }
+
+    ~board2() {
+        free(matrix);
+    }
+
+    void fill_random() {
+
+        // second row starting index
+        int start = m_width;
+        // last row starting index
+        int end = (m_height-1)*m_width;
+
+        // fill the inner part with random value
+        int line_count = 0;
+        for (int i = start; i < end ; ++i) {
+            // (line_count == 0 || line_count == m_width-1) => boarder
+            matrix[i] = (line_count == 0 || line_count == m_width-1) ? -1 : rand()%2;
+            line_count = (line_count+1)%m_width;
+        }
+
+        // fill left and right border
+        int left, right;
+        for (int i = 1; i < m_height ; i++) {
+            left = i*m_width;
+            right = left+m_width-1;
+            matrix[left] = matrix[right-1];
+            matrix[right] = matrix[left+1];
+        }
+
+        for (int j = 0; j < start; ++j) {
+            // copy last row in first row
+            matrix[j] = matrix[end-m_width+j];
+            // copy first row in last row
+            matrix[end+j] = matrix[start+j];
+        }
+
+
+    }
+
+    void print() {
+        auto line_count = 0;
+        for (auto i = 0; i < m_width*m_height; ++i) {
+
+            std::cout << matrix[i] << " ";
+            line_count++;
+
+            if (line_count == m_width){
+                std::cout << std::endl;
+                line_count = 0;
+            }
+
+        }
+        std::cout << std::endl;
+    }
+
+};
+
+#endif //GOL_BOARD2_H
---
+++ sequential2.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <chrono>
+
+#include "board2.h"
+
+/*
+ * compile with: g++ -std=c++11 -O3 sequential2.cpp -o seq2.exe
+ */
+
+//#define PRINT
+
+int main(int argc, char* argv[]) {
+
+    // board size
+    auto rows = atoi(argv[1]);
+    auto cols = atoi(argv[2]);
+    // iteration number
+    auto it_num = atoi(argv[3]);
+    // starting configuration
+    //auto conf_num = (argc>4) ? atoi(argv[4]) : 0;
+
+    // data structures
+    board2 in(rows,cols);
+    board2 out(rows,cols);
+    // data pointers
+    board2* p_in = &in;
+    board2* p_out = &out;
+    // matrix data pointer;
+    int* matrix_in;
+    int* matrix_out;
+
+//    switch (conf_num) {
+//        case 0:
+//            game_conf::set_random_conf(in);
+//            break;
+//        case 1 :
+//            game_conf::set_test_conf_1(in);
+//            break;
+//        case 2 :
+//            game_conf::set_test_conf_2(in);
+//            break;
+//        case 3:
+//            game_conf::set_test_conf_3(in);
+//            break;
+//        case 4:
+//            game_conf::set_test_conf_4(in);
+//            break;
+//    }
+#ifdef PRINT
+    in.print();
+#endif
+
+    // time start
+    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+
+    // game iteration
+    for (int k = 0; k < it_num; ++k) {
+
+        // current, upper and lower indices
+        auto up_p = 1;
+        auto curr_p = in.m_width+1;
+        auto low_p = in.m_width*2+1;
+
+        matrix_in = p_in->matrix;
+        matrix_out = p_out->matrix;
+
+        for (int i = 1; i <= rows * cols; ++i) {
+
+            auto sum = matrix_in[up_p-1] + matrix_in[up_p] + matrix_in[up_p+1]
+                        + matrix_in[curr_p-1] + matrix_in[curr_p+1]
+                        + matrix_in[low_p-1] + matrix_in[low_p] + matrix_in[low_p+1];
+
+            // empty cell
+            if (matrix_in[curr_p] == 0) {
+                // with exactly 3 alive neighbours alive otherwise keep empty
+                matrix_out[curr_p] = (sum == 3) ? 1 : 0;
+            } else if (matrix_in[curr_p] == 1) {
+                // with less then 2 or more than 3 alive neighbours then die
+                // otherwise keep alive
+                matrix_out[curr_p] = (sum < 2 || sum > 3) ? 0 : 1;
+            }
+
+            //std::cout << up_p << ", " << curr_p << ", " << low_p << std::endl;
+
+            up_p = (i%cols == 0) ? up_p+3 : up_p+1;
+            curr_p = (i%cols == 0) ? curr_p+3 : curr_p+1;
+            low_p = (i%cols == 0) ? low_p+3 : low_p+1;
+
+        }
+
+#ifdef PRINT
+        (*p_out).print();
+        std::cout << std::endl;
+#endif
+
+        // fill left and right border
+        int left, right;
+        for (int i = 1; i < in.m_height ; i++) {
+            left = i*in.m_width;
+            right = left+in.m_width-1;
+            matrix_out[left] = matrix_out[right-1];
+            matrix_out[right] = matrix_out[left+1];
+        }
+
+        // second row starting index
+        int start = in.m_width;
+        // last row starting index
+        int end = (in.m_height-1)*in.m_width;
+        for (int j = 0; j < start; ++j) {
+            // copy last row in first row
+            matrix_out[j] = matrix_out[end-in.m_width+j];
+            // copy first row in last row
+            matrix_out[end+j] = matrix_out[start+j];
+        }
+
+        // swap pointer
+        board2* tmp = p_in;
+        p_in = p_out;
+        p_out = tmp;
+
+    }
+
+
+    // time end
+    std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+
+    std::cout << std::endl;
+    std::cout << "game execution time is: " << duration << " milliseconds" << std::endl;
+    std::cout << std::endl;
+
+    return 0;
+}
